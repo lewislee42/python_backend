87ebca9c9ff8a49fb07bd4365456f5d900e90061
Lewis lee <lewlee@student.42kl.edu.my>
2023-10-15 15:37:33 +0800

    added number to words function

--- auth.py
+++ auth.py
@@ -162,6 +162,23 @@ def getStudentByLogin(login):
 		return response.json()
 
 
+@anvil.server.callable
+def numberTWords(number):
+    digit_to_word = {
+        '0': 'zero',
+        '1': 'one',
+        '2': 'two',
+        '3': 'three',
+        '4': 'four',
+        '5': 'five',
+        '6': 'six',
+        '7': 'seven',
+        '8': 'eight',
+        '9': 'nine'
+    }
+    return ''.join([digit_to_word[digit] for digit in str(number)])
+
+
 # # print(getStudentByLogin('lewlee'))
 # # print(getStudentByLogin('zhwong'))
 # # print(getStudentByLogin('hang'))
